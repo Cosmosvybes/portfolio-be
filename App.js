f53153ab62b425e373898b0495b21c42e884d54b
Cosmos <Alfredchrisayo@gmail.com>
2023-11-16 18:23:19 +0000

    add client side

--- App.js
+++ App.js
@@ -2,10 +2,17 @@ const express = require("express");
 const PORT = process.env.PORT || 1212;
 const { urlencoded } = require("body-parser");
 const app = express();
+const path = require("path");
 
 app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 const { ContactMeRoute } = require("./controller/Index");
+const exp = require("constants");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.post("/api/contact/me/message", ContactMeRoute);
 
