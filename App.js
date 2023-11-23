0dbf4fc04e7a1b635417fb79d44c5c29f4046283
Cosmos <Alfredchrisayo@gmail.com>
2023-11-23 17:30:29 +0000

    changes

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ app.use(urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 const { ContactMeRoute } = require("./controller/Index");
-const exp = require("constants");
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
